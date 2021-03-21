2c07ad79787c95422e8621684b48faa31505df77
Fredrik Almås <fredralm@stud.ntnu.no>
2021-03-21 20:55:24 +0100

    remove extra conv layer

--- assignment4/SSD/ssd/modeling/backbone/basic.py
+++ assignment4/SSD/ssd/modeling/backbone/basic.py
@@ -25,13 +25,6 @@ class BasicModel(torch.nn.Module):
             nn.Conv2d(
                 in_channels= image_channels,
                 out_channels= 128,
-                kernel_size=5,
-                stride=1,
-                padding=2
-            ),
-            nn.Conv2d(
-                in_channels= 128,
-                out_channels= 128,
                 kernel_size=3,
                 stride=1,
                 padding=1
