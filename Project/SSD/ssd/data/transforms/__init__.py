5ce4a193875e3d4d83e4b9530ae916d25c9193b7
Fredrik Almås <fredralm@stud.ntnu.no>
2021-04-19 13:36:00 +0200

    add augmentation and fix strides in config

--- Project/SSD/ssd/data/transforms/__init__.py
+++ Project/SSD/ssd/data/transforms/__init__.py
@@ -7,9 +7,9 @@ def build_transforms(cfg, is_train=True):
     if is_train:
         transform = [
             ConvertFromInts(),
+            RandomSampleCrop(),
+            RandomMirror(),
             ToPercentCoords(),
-            #RandomSampleCrop(),
-            #RandomMirror(),
             Resize(cfg.INPUT.IMAGE_SIZE),
             SubtractMeans(cfg.INPUT.PIXEL_MEAN, cfg.INPUT.PIXEL_STD),
             ToTensor(),
